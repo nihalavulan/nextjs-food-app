5ca67484e2bb9b0d3d71f6d20da17ae5f9a07672
Nihal-a <nihalavulan1@gamil.com>
2022-05-03 00:17:32 +0530

    Created featured pizza section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../components/Featured'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
@@ -11,7 +12,7 @@ export default function Home() {
         <meta name="description" content="Studying next js with project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      hello guys
+      <Featured />
     </div>
   )
 }
